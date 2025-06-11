ce2534270c8c2e0c22d8384f20eb7d06754fc86d
udasan <udasan.koubou@gmail.com>
2025-06-11 12:03:05 +0900

    Hashilus.Helper を更新（マウスカーソルを消すやつ）

---
+++ Assets/Hashilus.Helper/CursorSleeper.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Hashilus.CursorSleeper
+{
+    public class CursorSleeper : MonoBehaviour
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+        static void Initialize()
+        {
+            var cursorSleeper = new GameObject(nameof(CursorSleeper)).AddComponent<CursorSleeper>();
+            DontDestroyOnLoad(cursorSleeper.gameObject);
+        }
+
+        const float ElapsedSecondsNeededForSleep = 2f;
+        float idleSeconds;
+        Vector3 previousMousePosition;
+
+        void Update()
+        {
+            var mousePosition = Mouse.current.position.ReadValue();
+            var currentMousePosition = new Vector3(mousePosition.x, mousePosition.y, 0);
+            if (currentMousePosition == previousMousePosition)
+            {
+                idleSeconds += Time.deltaTime;
+            }
+            else
+            {
+                idleSeconds = 0;
+            }
+
+            var sleeping = idleSeconds > ElapsedSecondsNeededForSleep;
+            Cursor.visible = !sleeping;
+
+            previousMousePosition = currentMousePosition;
+        }
+    }
+}
---
+++ Assets/Hashilus.Helper/PolygonCounter.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Hashilus.Helper
+{
+    public class PolygonCounter : MonoBehaviour
+    {
+        public bool includeMeshFilter = true;
+        public bool includeSkinnedMeshRenderer = true;
+        public bool includeInactive = false;
+    }
+
+#if UNITY_EDITOR
+    [CustomEditor(typeof(PolygonCounter))]
+    public class PolygonCounterEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            var polys = 0;
+
+            var polygonCounter = target as PolygonCounter;
+            if (polygonCounter.includeMeshFilter) polys += GetMeshFilterPolygons(polygonCounter.includeInactive);
+            if (polygonCounter.includeSkinnedMeshRenderer) polys += GetSkinnedMeshRendererPolygons(polygonCounter.includeInactive);
+
+            EditorGUILayout.LabelField("Triangles: " + polys.ToString());
+        }
+
+        int GetMeshFilterPolygons(bool includeInactive = false)
+        {
+            var filters = (target as Component).GetComponentsInChildren<MeshFilter>(includeInactive);
+            return filters.Sum(filter => filter.sharedMesh.triangles.Length) / 3;
+        }
+
+        int GetSkinnedMeshRendererPolygons(bool includeInactive = false)
+        {
+            var skins = (target as Component).GetComponentsInChildren<SkinnedMeshRenderer>(includeInactive);
+            return skins.Sum(skin => skin.sharedMesh.triangles.Length) / 3;
+        }
+    }
+#endif
+}
